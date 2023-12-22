932163fe5cf6072937943b22a2b0a832b4e15b08
William Stoneham <58570245+RockBacon9922@users.noreply.github.com>
2023-12-22 21:06:33 +0000

    all working

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,17 @@ use raylib::drawing::RaylibDraw;
 
 fn main() {
     println!("Hello, world!");
-    let window_width:i32 = 500;
-    let window_height:i32 = 500;
-    let (mut rl, thread) = raylib::init().size(window_width, window_height).title("Game").build();
+    let window_width: i32 = 500;
+    let window_height: i32 = 500;
+    let (mut rl, thread) = raylib::init()
+        .size(window_width, window_height)
+        .title("Game")
+        .build();
+    rl.set_target_fps(60);
     while !rl.window_should_close() {
         let mut d = rl.begin_drawing(&thread);
         d.clear_background(Color::WHITE);
+        d.draw_rectangle(5, 5, 100, 100, Color::RED);
         d.draw_fps(30, 30);
     }
 }
